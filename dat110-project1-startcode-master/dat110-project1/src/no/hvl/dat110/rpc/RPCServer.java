fd5aedf887684dd0b58009b2645d0d681be87596
571530 <truls.martinsen13@gmail.com>
2019-01-30 15:48:46 +0100

    Ferdig med task 2

--- dat110-project1-startcode-master/dat110-project1/src/no/hvl/dat110/rpc/RPCServer.java
+++ dat110-project1-startcode-master/dat110-project1/src/no/hvl/dat110/rpc/RPCServer.java
@@ -8,61 +8,69 @@ import no.hvl.dat110.messaging.MessagingServer;
 
 public class RPCServer {
 
-	private MessagingServer msgserver;
-	private Connection connection;
-	
-	// hashmap to register RPC methods which are required to implement RPCImpl
-	
-	private HashMap<Integer,RPCImpl> services;
-	
-	public RPCServer(int port) {
-		
-		this.msgserver = new MessagingServer(port);
-		this.services = new HashMap<Integer,RPCImpl>();
-		
-		// the stop RPC methods is built into the server
-		services.put((int)RPCCommon.RPIDSTOP,new RPCServerStopImpl());
-	}
-	
-	public void run() {
-		
-		System.out.println("RPC SERVER RUN - Services: " + services.size());
-		
-		connection = msgserver.accept(); 
-		
-		System.out.println("RPC SERVER ACCEPTED");
-		
-		boolean stop = false;
-		
-		while (!stop) {
-	    
-		   int rpcid;
-		   
-		   // TODO
-		   // - receive message containing RPC request
-		   // - find the identifier for the RPC methods to invoke
-		   // - lookup the methods to be invoked
-		   // - invoke the method
-		   // - send back message containing RPC reply
-			
-		   if (true) {
-		     throw new RuntimeException("not yet implemented");
-		   }
-		   
-		   if (rpcid == RPCCommon.RPIDSTOP) {
-			   stop = true;
-		   }
-		}
-	
-	}
-	
-	public void register(int rmid, RPCImpl impl) {
-		services.put(rmid, impl);
-	}
-	
-	public void stop() {
-		connection.close();
-		msgserver.stop();
-		
-	}
+    private MessagingServer msgserver;
+    private Connection connection;
+
+    // hashmap to register RPC methods which are required to implement RPCImpl
+
+    private HashMap<Integer, RPCImpl> services;
+
+    public RPCServer(int port) {
+
+        this.msgserver = new MessagingServer(port);
+        this.services = new HashMap<Integer, RPCImpl>();
+
+        // the stop RPC methods is built into the server
+        services.put((int) RPCCommon.RPIDSTOP, new RPCServerStopImpl());
+    }
+
+    public void run() {
+
+        System.out.println("RPC SERVER RUN - Services: " + services.size());
+
+        connection = msgserver.accept();
+
+        System.out.println("RPC SERVER ACCEPTED");
+
+        boolean stop = false;
+
+        while (!stop) {
+
+            int rpcid;
+
+            byte[] data = connection.receive().getData();
+
+            rpcid = Byte.toUnsignedInt(data[0]);
+
+            RPCImpl impl = services.get(rpcid);
+
+            if (impl != null) {
+		   	    Message message = new Message(impl.invoke(data));
+		   	    connection.send(message);
+            }
+
+            // TODO
+            // - receive message containing RPC request
+            // - find the identifier for the RPC methods to invoke
+            // - lookup the methods to be invoked
+            // - invoke the method
+            // - send back message containing RPC reply
+
+
+            if (rpcid == RPCCommon.RPIDSTOP) {
+                stop = true;
+            }
+        }
+
+    }
+
+    public void register(int rmid, RPCImpl impl) {
+        services.put(rmid, impl);
+    }
+
+    public void stop() {
+        connection.close();
+        msgserver.stop();
+
+    }
 }
--- dat110-project1-startcode-master/dat110-project1/src/no/hvl/dat110/rpc/RPCUtils.java
+++ dat110-project1-startcode-master/dat110-project1/src/no/hvl/dat110/rpc/RPCUtils.java
@@ -1,52 +1,40 @@
 package no.hvl.dat110.rpc;
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class RPCUtils {
 
 	public static byte[] marshallString(byte rpcid, String str) {
 
-		byte[] encoded;
+		byte[] encoded = new byte[1 + str.getBytes().length];
 
-		// TODO: marshall RPC identifier and string into byte array
+		encoded[0] = rpcid;
 
-		if (true) {
-			throw new RuntimeException("not yet implemented");
+		for (int i = 0; i < str.getBytes().length; i++) {
+			encoded[i+1] = str.getBytes()[i];
 		}
 
 		return encoded;
 	}
 
 	public static String unmarshallString(byte[] data) {
-
-		String decoded;
-
-		// TODO: unmarshall String contained in data into decoded
-
-		if (true) {
-			throw new RuntimeException("not yet implemented");
-		}
+		String decoded = new String(Arrays.copyOfRange(data, 1, data.length));
 
 		return decoded;
 	}
 
 	public static byte[] marshallVoid(byte rpcid) {
 
-		byte[] encoded;
-
-		// TODO: marshall RPC identifier in case of void type
-
-		if (true) {
-			throw new RuntimeException("not yet implemented");
-		}
+		byte[] encoded = new byte[1];
+		encoded[0] = rpcid;
 
 		return encoded;
 
 	}
 
 	public static void unmarshallVoid(byte[] data) {
-
-		// TODO: unmarshall void type
+		return;
 	}
 
 	public static byte[] marshallBoolean(byte rpcid, boolean b) {
@@ -72,12 +60,12 @@ public class RPCUtils {
 
 	public static byte[] marshallInteger(byte rpcid, int x) {
 
-		byte[] encoded;
+		byte[] encoded = new byte[5];
 
-		// TODO: marshall RPC identifier and string into byte array
+		encoded[0] = rpcid;
 
-		if (true) {
-			throw new RuntimeException("not yet implemented");
+		for (int i = 0; i < 4; i++) {
+			encoded[i + 1] = (byte) (x >> (i * 8));
 		}
 
 		return encoded;
@@ -85,12 +73,10 @@ public class RPCUtils {
 
 	public static int unmarshallInteger(byte[] data) {
 
-		int decoded;
+		int decoded = 0;
 
-		// TODO: unmarshall integer contained in data
-
-		if (true) {
-			throw new RuntimeException("not yet implemented");
+		for (int i = 0; i < 4; i++) {
+			decoded += Byte.toUnsignedInt(data[i + 1]) << (i * 8);
 		}
 
 		return decoded;
